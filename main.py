259f860cfedf9c7937adc321ee06b55ca1166992
nico-byte <nicofuchs653@outlook.de>
2025-10-21 22:50:21 +0200

    Using np.ones instead of np.random.randn for array creation in python matmul baseline

--- main.py
+++ main.py
@@ -4,15 +4,15 @@ import numpy as np
 
 def main():
     print("Hello from mojo-nn!")
-    a = np.random.randn(1024, 1024).astype(np.float32)
-    b = np.random.randn(1024, 1024).astype(np.float32)
+    a = np.ones((1024, 1024)).astype(np.float32)
+    b = np.ones((1024, 1024)).astype(np.float32)
 
     start = perf_counter_ns()
     c = np.matmul(a, b)
     end = perf_counter_ns()
 
     print(c)
-    print("Time taken:", end - start)
+    print("Time taken for matmul (numpy):", (end - start) / 1e6, "milliseconds")
 
 
 if __name__ == "__main__":
